995445d4bd4934bb63c2e173c918ed778de75b5c
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-06-06 15:53:46 +0530

    Create Increment.java

---
+++ Coading Ninjas/Lecture 8 || Function & Scope/Increment.java
@@ -0,0 +1,20 @@
+package lecture8;
+
+public class Increment {
+public static void increment(int n) {
+	n = n+1;
+	System.out.println(n);
+}
+	public static void main(String[] args) {
+int a = 10;
+increment(a);
+System.out.println(a);
+	}
+
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+//Output--
+// 11
+// 10
