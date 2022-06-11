164126ae58e38f4ba7521f0dc10c39d0416aaf9d
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-06-11 19:13:47 +0530

    Update and rename for loop to for_loop.java

---
+++ Coading Ninjas/Lecture 5 || loop & pattern/for_loop.java
@@ -0,0 +1,22 @@
+package lecture5;
+import java.util.Scanner;
+public class Forloophelloworld {
+
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	for (int i=0;i<5;i++) {
+		System.out.println("Hello world");
+	}
+	
+
+	}
+
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// Hello world
+// Hello world
+// Hello world
+// Hello world
+// Hello world
