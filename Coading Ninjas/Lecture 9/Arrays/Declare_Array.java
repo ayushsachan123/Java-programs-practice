8bbde8fd75390a6d471b513bb8c1a4b15b2943ae
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-06-14 22:48:42 +0530

    Create Declare_Array.java

---
+++ Coading Ninjas/Lecture 9/Arrays/Declare_Array.java
@@ -0,0 +1,22 @@
+package arrays;
+import java.util.Scanner;
+public class Declarearrays {
+
+	public static void main(String[] args) {
+	int arr[] = new int[10];
+	arr[0]=5;
+	arr[5]=17;
+	System.out.println(arr[0]);
+	System.out.println(arr[5]);
+int length = arr.length;
+System.out.println(length); // finding length of the array
+	}
+
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// 5
+// 17
+// 10
