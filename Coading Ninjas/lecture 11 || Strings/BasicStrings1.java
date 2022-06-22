20affbcc55d223b303a6661e62c7826e268bf84c
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-06-22 17:54:10 +0530

    Create BasicStrings1.java

---
+++ Coading Ninjas/lecture 11 || Strings/BasicStrings1.java
@@ -0,0 +1,31 @@
+package strings;
+import java.util.Scanner;
+public class Basic_Strings {
+
+	public static void main(String[] args) {
+		
+	char arr[]= {'a','y', 'u','s','h'};
+	String str = "coading";
+	
+	String str1 = "";  //empty string
+	String str2 = " is fun";
+	
+	System.out.println("Concatenation of string " + str +str2);//adding to string
+	
+	System.out.println("length of string str " + str.length());//length of the string
+	
+	System.out.println("length of string str1 " + str1.length());
+	
+	System.out.println(str.charAt(2));//printing the character at position 2
+
+	}
+
+}
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// Concatenation of string coading is fun
+// length of string str 7
+// length of string str1 0
+// a
