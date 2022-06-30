520fc55adbe2638c250118550ac9e19cc64ecc73
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-06-30 16:26:49 +0530

    Create How_2D_Array_Stored.java

---
+++ Coading Ninjas/lecture 12 || 2D array/How_2D_Array_Stored.java
@@ -0,0 +1,24 @@
+package _2D_Array;
+import java.util.Scanner;
+public class How_2D_Array_Stored {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int arr2d[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+System.out.println(arr2d.length);
+System.out.println(arr2d[0].length);
+System.out.println(arr2d);
+System.out.println(arr2d[0]);
+System.out.println(arr2d[1]);
+
+	}
+
+}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+//Output--
+// 4
+// [[I@1b28cdfa
+// [I@eed1f14
+// [I@7229724f
