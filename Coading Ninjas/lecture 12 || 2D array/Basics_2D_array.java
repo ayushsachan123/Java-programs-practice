a034d6dde9fe64d1f40871e09a035581331da498
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-06-30 16:25:23 +0530

    Create Basics_2D_array.java

---
+++ Coading Ninjas/lecture 12 || 2D array/Basics_2D_array.java
@@ -0,0 +1,19 @@
+package _2D_Array;
+import java.util.Scanner;
+public class Basics_2D_Array {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int[][] arr2d = new int[3][4];
+		arr2d[2][1] = 12;
+      System.out.println(arr2d[2][1]);
+      System.out.println(arr2d[1][1]);
+	}
+
+}
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+//Output--
+// 12
+// 0
